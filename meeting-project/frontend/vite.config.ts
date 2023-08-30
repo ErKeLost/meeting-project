78dbd2534208aa0f13ed2780b7219a8c86a2482b
erkelost <1256029807@qq.com>
2023-08-30 13:52:39 +0800

    chore: update struct

--- meeting-project/frontend/vite.config.ts
+++ meeting-project/frontend/vite.config.ts
@@ -14,13 +14,14 @@ export default defineConfig({
   },
   server: {
     host: true,
+    port: 3001,
     proxy: {
       "/api": {
         target: "http://localhost:3000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
-      }
-    }
+      },
+    },
   },
   plugins: [
     react(),
